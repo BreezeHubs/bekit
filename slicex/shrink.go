fe361a4fa1a80afbd1c40f7c4c8623a437570a96
iuR <2356665641@qq.com>
2023-01-05 18:16:49 +0800

    ArrayShrink

--- slicex/shrink.go
+++ slicex/shrink.go
@@ -22,6 +22,10 @@ func calcCapacity(c, l int) (int, bool) {
 }
 
 // ArrayShrink 缩容成新数组
+// 在必要的时候执行缩容，其算法为：
+// - 如果容量 > 2048，并且长度小于容量一半，那么就会缩容为原本的 5/8
+// - 如果容量 (64, 2048]，如果长度是容量的 1/4，那么就会缩容为原本的一半
+// - 如果此时容量 <= 64，那么我们将不会执行缩容。在容量很小的情况下，浪费的内存很少，所以没必要消耗 CPU去执行缩容
 func ArrayShrink[T any](src []T) []T {
 	n, changed := calcCapacity(cap(src), len(src))
 	if !changed {
